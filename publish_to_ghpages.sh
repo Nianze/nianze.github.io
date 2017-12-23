4b61df3c7fbc2a221981c4218ff8cd80f860f974
Nzo <ez@Ez-MacBook-2017.local>
2017-12-23 23:14:12 +0800

    udpate script

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -9,11 +9,10 @@ fi
 echo "Deleting old publication"
 rm -rf public
 mkdir public
-git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -23,3 +22,6 @@ hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "Push to origin"
+git push origin gh-pages
