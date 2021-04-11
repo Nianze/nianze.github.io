0122548d193e8963d7ad02be22673c81f2b39d2b
Nianze <nl443@cornell.edu>
2021-04-11 04:02:28 -0400

    Publishing to gh-pages (publish.sh)

---
+++ images/2021/04/11/filmWeeklyGen.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# run the following in scripts dir
+
+TYPE="film"
+
+HOMEDIR=$(dirname $(pwd))
+IMGDATEPATH=$(date +'%Y/%m/%d')
+IMGDIR=$HOMEDIR/static/gallery/$TYPE/$IMGDATEPATH
+THUMBNAILDIR=$HOMEDIR/static/images/$IMGDATEPATH
+POSTDIR=$HOMEDIR/content/gallery/$TYPE/$(date +'%Y/%m/%d')
+POSTNAME=$(date +'%Y-%m-%d')-weekly.md
+POSTNAME_ZH=$(date +'%Y-%m-%d')-weekly.zh.md
+POST=$POSTDIR/$POSTNAME
+POST_ZH=$POSTDIR/$POSTNAME_ZH
+
+cd $IMGDIR
+mkdir -p $THUMBNAILDIR
+find . -type f -exec cp {} $THUMBNAILDIR \;
+cd $THUMBNAILDIR
+sips -Z 1200 *
+cd $HOMEDIR
+hugo new --kind weekly gallery/$TYPE/$(date +'%Y/%m/%d')/$POSTNAME
+hugo new --kind weekly gallery/$TYPE/$(date +'%Y/%m/%d')/$POSTNAME_ZH
+
+d1=$(date -jf "%Y-%m-%d %H:%M" "2019-05-04 00:00" +%s)
+d2=$(date +%s)
+WEEKCNT=$(( (d2 - d1) / 86400 / 7 + 1)) 
+
+sed -i '' "s|title:.*|title: \"[Weekly-$WEEKCNT] $(date +'%Y.%m.%d')\"|" $POST
+sed -i '' "s|title:.*|title: \"[Weekly-$WEEKCNT] $(date +'%Y.%m.%d')\"|" $POST_ZH
+sed -i '' "s|featured_image:.*|featured_image: $IMGDATEPATH\/???.jpg|" $POST
+sed -i '' "s|featured_image:.*|featured_image: $IMGDATEPATH\/???.jpg|" $POST_ZH
+lf=$'\n'
+sed -i '' "s|<!--more-->|Weekly visual project - $(date +'%Y/%m/%d')\\$lf&|" $POST
+sed -i '' "s|<!--more-->|Weekly visual project - $(date +'%Y/%m/%d')\\$lf&|" $POST_ZH
+
+# echo '' >> $POST
+# echo '# The excerpt' >> $POST
+# echo '> ...   ' >> $POST
+# echo 'Excerpt From: ' >> $POST
+
+exit 0;
